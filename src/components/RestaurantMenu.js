f5edd8bc9e844faa535dfe65f28115f48a8fcf0f
Dinesh Yadav <dineshinau@gmail.com>
2026-09-21 20:15:50 +0000

    Add new JSON file for utility data (8614.json)

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,31 +2,32 @@ import { useEffect, useState } from "react";
 import {getMenuById} from "../utils/menu"
 import { useParams } from "react-router-dom";
 
-const RestaurantMenu = (data ) => {
+const RestaurantMenu = () => {
     const [resData, setResData] = useState([]);
     useEffect( () => {
         fetchMenu();
     },[]);
 
-    const {id}  = data;
     const {restId} = useParams();
 
     const fetchMenu = async () => {
         try{
             const data = await getMenuById(restId);
+            // console.log(data);
+
             setResData(data?.data?.cards);
         }catch(err){
             console.log(err);
         }
     }
 
-    return ( resData?.length < 1 ? 'No restaurant found' :
+    return ( resData?.length < 1 ? (<div className="no-data"><p className="no-data-msg">JSON Data for the restaurant is not added yet. Please check anoter restaurant.</p></div>) :
         (<div className="resto-menu">
             <h3>{resData[0]?.card?.card?.text}</h3>
             <p>{resData[2]?.card?.card?.info?.cuisines.join(", ")}</p>
             <h2>Menu</h2>
             <ul>
-                {resData[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards.map((items) => <li key={items?.card?.info?.id}>{items?.card?.info?.name} - ₹ {(items?.card?.info?.price)/100}</li> )}
+                {resData[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards.map((items) => <li key={items?.card?.info?.id}>{items?.card?.info?.name} - ₹ {(items?.card?.info?.price || items?.card?.info?.defaultPrice)/100}</li> )}
             </ul>
         </div>
         )
--- src/utils/menu.js
+++ src/utils/menu.js
@@ -1,16 +1,6 @@
-import menu51510 from "./51510.json";
-import menu242282 from "./242282.json";
-import menu467579 from "./467579.json";
-import menu8620 from "./8620.json";
+// src/utils/menu.js
+import menus from "./*.json";
 
-const menus = {
-  51510: menu51510,
-  242282: menu242282,
-  467579: menu467579,
-  8620: menu8620,
-};
-
-// async so your component code looks like a real API call
 export const getMenuById = async (id) => {
   const data = menus[id];
   if (!data) throw new Error(`No menu found for restaurant ${id}`);
