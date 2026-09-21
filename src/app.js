cd5e190e5e6e92c9002928360c2d637ed092e759
Dinesh Yadav <dineshinau@gmail.com>
2026-09-21 10:34:18 +0000

    feat: implement routing in Header component and update AppLayout for nested routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -14,7 +14,7 @@ const AppLayout = () => {
     return (
         <div className="app">
             <Header />
-            <Body />
+            <Outlet />
             <Footer />
         </div>
     )
@@ -24,16 +24,22 @@ const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <AppLayout />,
-        errorElement: <Error />
+        errorElement: <Error />,
+        children: [
+            {
+                path: "/",
+                element: <Body />
+            },
+           {
+                path: "/about",
+                element: <About />
+            },
+            {
+                path: "/contact",
+                element: <Contact />
+            }
+        ]
     },
-    {
-        path: "/about",
-        element: <About />
-    },
-    {
-        path: "/contact",
-        element: <Contact />
-    }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { LOGO_URL } from "../utils/constant";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     const [btnText, setBtnText] = useState('Login')
@@ -18,9 +19,9 @@ const Header = () => {
             </div>
             <div className="nav-links">
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                     <li>Cart</li>
                     <button onClick={() => {
                         setBtnText('Login' === btnText ? 'Logout' : 'Login')
